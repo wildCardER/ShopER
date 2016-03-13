25434e8b4da7080d9ab196c73a382e5a076a4f39
solvib13@ru.is <solvib13@ru.is>
2016-03-13 17:15:04 +0000

    Product card added

--- src/components/product/ProductsController.js
+++ src/components/product/ProductsController.js
@@ -3,7 +3,7 @@
 angular.module("project3App").controller("ProductsController",
 function ProductsController($scope, AppResource, $routeParams) {
 
-	AppResource.getSellerProducts($routeParams.id).success(function(product){
+	AppResource.getSellerProducts(parseInt($routeParams.id)).success(function(product){
 		$scope.products = product;
 	}).error(function(){
 		console.log("No products to display");
